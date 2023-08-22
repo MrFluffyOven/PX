fe47eac00899b904ebc54f60c16177245c33d57d
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-08-22 10:47:52 +0100

    Update twrp_gta8wifi.mk

--- twrp_gta8wifi.mk
+++ twrp_gta8wifi.mk
@@ -5,25 +5,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 Release name
+# Release name
 PRODUCT_RELEASE_NAME := gta8wifi
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from gta8wifi device
-$(call inherit-product, device/samsung/gta8wifi/device.mk)
-
-PRODUCT_DEVICE := gta8wifi
-PRODUCT_NAME := twrp_gta8wifi
-PRODUCT_BRAND := samsung
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
 PRODUCT_MODEL := SM-X200
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)/device.mk)
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
